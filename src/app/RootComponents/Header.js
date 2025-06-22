fafb76581140ea1bba89665e356504a34ac0c376
Charlotte Lee <charlotteleenyc@gmail.com>
2025-06-22 04:24:30 -0400

    added header and homepage

--- src/app/RootComponents/Header.js
+++ src/app/RootComponents/Header.js
@@ -1,15 +1,31 @@
 "use client";
 
 import React from "react";
-import Link from "next/link"
-import {useRouter} from "next/navigation"
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
   const router = useRouter();
+
     return (
-        <nav className="flex justify-around items-center bg-black h-13 text-white">
-          <Link href = '/'>Home</Link>
-          <Link href = '/map'>Map</Link>
+    <nav className="relative flex items-center bg-white text-black h-[100px] px-4 border-b border-gray-300">
+      {/* Logo at left, vertically centered */}
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
+        <Image
+          src="/logo.png"
+          alt="Path4You logo"
+          width={140}
+          height={140}
+          className="object-contain"
+        />
+      </div>
+
+      {/* Centered navigation links */}
+      <div className="mx-auto flex space-x-100 text-xl font-medium">
+        <Link href="/">Home</Link>
+        <Link href="/map">Map</Link>
+      </div>
         </nav>
     );
 }
